fca743affd20430c225a42e97a284a2a612a00c1
laki bonjour <adflaki@gmail.com>
2021-01-17 14:47:35 +0100

    add hashing password

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 const app = express();
 
 app.use(express.static("public"));
@@ -22,7 +22,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
@@ -32,7 +31,7 @@ app.route("/register")
     .post((req, res) => {
         const newUser = new User({
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password)
         });
         newUser.save((err) => {
             if (err) {
@@ -54,13 +53,15 @@ app.route("/login")
     .post((req, res) => {
         User.findOne({
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password)
         }, (err, result) => {
             if (err) {
                 console.log(err);
             } else {
-                console.log(result);
-                res.render("secrets");
+                if (result) {
+                    console.log(result);
+                    res.render("secrets");
+                }
             }
         })
     });
